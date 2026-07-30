77e66b7c64a051be28abb88464853d3d4969f15a
mauricioburgos11-dataanalyst <mauricioburgos11@gmail.com>
2026-07-30 18:46:45 -0300

    Update farmacia_sql.py

--- farmacia_sql.py
+++ farmacia_sql.py
@@ -3,6 +3,7 @@ import database  # Reutilizamos la conexión con SSL a Aiven
 
 # 1. ESQUEMA DE LA BASE DE DATOS (Se lo enviamos a Gemini para que sepa qué consultar)
 ESQUEMA_FARMACIA = """
+
 Tablas disponibles en la base de datos 'defaultdb':
 
 1. Tabla 'accion_terapeutica' (
